9ddf0e4e41d28b352bb3affd4ada91613d333a79
Melanie Cabalu <micabalu@cpp.edu>
2016-01-19 23:06:47 -0800

    first commit

---
+++ app/src/main/java/com/example/melanie/ideal/Details.java
@@ -0,0 +1,56 @@
+package com.example.melanie.ideal;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+
+public class Details extends AppCompatActivity {
+
+    private Button buyNowButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        buyNowButton = (Button) findViewById(R.id.buyNowButton);
+        buyNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.amazon.com/gp/product/B0019M7WCW/ref=as_li_ss_tl?ie=UTF8&smid=ATVPDKIKX0DER&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-6&pf_rd_r=0SQ0A02TQWMXB37N3MQM&pf_rd_t=101&pf_rd_p=23f4bf1c-9a1e-4911-9e6a-6409e14a00e7&pf_rd_i=13633803011&linkCode=sl1&tag=fsf206-20&linkId=330f0597a4205eaa1656cb7d13f17c8c");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
